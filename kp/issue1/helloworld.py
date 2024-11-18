4ee04d7d561fcb56884831afbeecc184ef5b83bf
KP <head.iie@vnrvjiet.in>
2024-11-18 17:52:48 +0530

    Code for Basic Setup (Issue1) documentation #1

--- kp/issue1/helloworld.py
+++ kp/issue1/helloworld.py
@@ -1,19 +1,22 @@
 
-import math
+import math  # part of Task4
 
+# Task1
 print ("Hello World")
 
+# Task2
 def initGraphics():
     print("Graphics Initialised.")
 
 initGraphics()
 
-
+# Task3
 def runInLoops():
     for gpu_core in range(1,10):
         print (f"GPU Core {gpu_core} is active.")
     
 runInLoops()
 
+# Task4
 print(f" Which is greater 10^4 != 4^10? Hint : 4^8 is {math.pow(4,8)}")
 
